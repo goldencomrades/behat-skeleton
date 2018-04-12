494270d7d3f014ae3fed129a1078ad6ca246f44d
Francesco Bianco <info.francescobianco@gmail.com>
2018-04-12 17:09:43 +0200

    fix command line utils

--- bdd.sh
+++ bdd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "See in action at [http://testing.localhost:8081] or press [Enter]"
+read -t 3
+
 docker-compose exec php ./vendor/bin/behat --init --no-colors
 docker-compose exec php ./vendor/bin/behat \
     features/suites/$1/$2.feature \
